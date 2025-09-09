b861a65d7481d861750f010ad39ff804166d4dcc
Chris J Arges <carges@cloudflare.com>
2025-09-08 21:09:26 -0500

    Add sfq.

--- tests.sh
+++ tests.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 source ./venv/bin/activate
 
+TIME=60
+
 echo "=== NO QDISC ==="
 ip netns exec client tc qdisc del dev to-server root || true
-ip netns exec client bbperf -u -c 192.168.20.2 -B 192.168.20.1 -g
+ip netns exec client bbperf -t $TIME -u -c 192.168.20.2 -B 192.168.20.1 -g
 
 echo "=== QDISC FQ_CODEL  ==="
 ip netns exec client tc qdisc replace dev to-server root fq_codel
-ip netns exec client bbperf -u -c 192.168.20.2 -B 192.168.20.1 -g
+ip netns exec client bbperf -t $TIME -u -c 192.168.20.2 -B 192.168.20.1 -g
+
+echo "=== QDISC SFQ ==="
+ip netns exec client tc qdisc replace dev to-server root sfq
+ip netns exec client bbperf -t $TIME -u -c 192.168.20.2 -B 192.168.20.1 -g
