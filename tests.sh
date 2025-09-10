a0e720768bc5218f9a47909ded7b0d0fe47c2c68
Chris J Arges <carges@cloudflare.com>
2025-09-10 16:38:29 -0500

    Simplify tests.

--- tests.sh
+++ tests.sh
@@ -3,17 +3,20 @@ source ./venv/bin/activate
 
 TIME=60
 
+run_test() {
+  output=$1
+  ip netns exec client bbperf -t $TIME -u -c 192.168.20.2 -B 192.168.20.1 -g -J $output.json | grep "created graph"
+  ip netns exec client tc -s qdisc ls dev to-server
+}
+
 echo "=== NO QDISC ==="
 ip netns exec client tc qdisc del dev to-server root || true
-ip netns exec client bbperf -t $TIME -u -c 192.168.20.2 -B 192.168.20.1 -g
-ip netns exec client tc -s qdisc ls dev to-server
+run_test "no_qdisc"
 
 echo "=== QDISC FQ_CODEL  ==="
 ip netns exec client tc qdisc replace dev to-server root fq_codel
-ip netns exec client bbperf -t $TIME -u -c 192.168.20.2 -B 192.168.20.1 -g
-ip netns exec client tc -s qdisc ls dev to-server
+run_test "fq_codel"
 
 echo "=== QDISC SFQ ==="
 ip netns exec client tc qdisc replace dev to-server root sfq
-ip netns exec client bbperf -t $TIME -u -c 192.168.20.2 -B 192.168.20.1 -g
-ip netns exec client tc -s qdisc ls dev to-server
+run_test "sfq"
