28030313b140c935f4715b9f8d9ed3aa6c0f49e0
Jesper Dangaard Brouer <jesper@cloudflare.com>
2025-10-24 16:20:51 +0200

    setup.sh: disable veth multiqueue setup per default
    This is to make sure our ping packets travels in the same queue
    as the bbperf UDP traffic.  With multiqueue the ping packets
    might not get affected by the UDP packets (if it ends up travling
    on a different queue).  Our goal is to measure if ping packets
    gets influenced by the bbperf UDP traffic.
    
    Signed-off-by: Jesper Dangaard Brouer <jesper@cloudflare.com>

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
+#MQ=yes
+MQ=no
+
 # recreate namespaces
 ip netns del server || true
 ip netns del client || true
@@ -10,8 +13,13 @@ ip netns add client
 ip netns add router
 
 # setup routing between netns namespaces
-ip -netns client link add dev to-router type veth peer name client-link netns router numtxqueues 8 numrxqueues 8
-ip -netns server link add dev in-router type veth peer name server-link netns router numtxqueues 8 numrxqueues 8
+if [[ "${MQ}" == "yes" ]]; then
+  ip -netns client link add dev to-router type veth peer name client-link netns router numtxqueues 8 numrxqueues 8
+  ip -netns server link add dev in-router type veth peer name server-link netns router numtxqueues 8 numrxqueues 8
+else
+  ip -netns client link add dev to-router type veth peer name client-link netns router
+  ip -netns server link add dev in-router type veth peer name server-link netns router
+fi
 
 # bring up devices and assign IPs
 #
